752b712e0e987b619ac26959ad852fce89b2437a
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-04-07 05:47:37 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,19 @@
 class Dog
-  #attr_accessor :name, :breed
-  fido = Dog.new
+  attr_accessor :name, :breed
+
   def initialize(name, breed="Mutt")
     @name = name
     @breed = breed
   end
 
 
-  def name=(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
+  # def name=(name)
+  #   @name = name
+  # end
+  #
+  # def name
+  #   @name
+  # end
 
   def bark
     puts "woof!"
